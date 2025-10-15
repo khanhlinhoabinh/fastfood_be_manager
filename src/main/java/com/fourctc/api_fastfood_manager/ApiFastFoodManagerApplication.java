b3d4f5259fe86e75c2700033add2e80495113902
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-10-15 10:37:22 +0700

    first commit

---
+++ src/main/java/com/fourctc/api_fastfood_manager/ApiFastFoodManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fourctc.api_fastfood_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFastFoodManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFastFoodManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fourctc/api_fastfood_manager/ApiFastFoodManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourctc.api_fastfood_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFastFoodManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
