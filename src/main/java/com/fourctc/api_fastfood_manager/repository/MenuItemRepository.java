66ba80e5d5a61adcf8e0f0bbf891d718b3b33aeb
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-10-16 11:23:11 +0700

    sap xep mon

--- src/main/java/com/fourctc/api_fastfood_manager/repository/MenuItemRepository.java
+++ src/main/java/com/fourctc/api_fastfood_manager/repository/MenuItemRepository.java
@@ -3,9 +3,11 @@ package com.fourctc.api_fastfood_manager.repository;
 import com.fourctc.api_fastfood_manager.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Sort;
+import java.util.List;
 @Repository
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
     // JpaRepository đã có sẵn các hàm CRUD: findAll(), findById(), save(), deleteById()
-
+    List<MenuItem> findAll(Sort sort);
 }
