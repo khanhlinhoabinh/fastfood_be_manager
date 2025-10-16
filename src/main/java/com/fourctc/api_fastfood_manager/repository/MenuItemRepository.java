454d03dd49cae61fcc391048be1c0e55357995d4
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-10-16 10:28:46 +0700

    repository cua lay danh sach mon menuitem

--- src/main/java/com/fourctc/api_fastfood_manager/repository/MenuItemRepository.java
+++ src/main/java/com/fourctc/api_fastfood_manager/repository/MenuItemRepository.java
@@ -1,2 +1,11 @@
-package com.fourctc.api_fastfood_manager.repository;public class MenuItemRepository {
+package com.fourctc.api_fastfood_manager.repository;
+
+import com.fourctc.api_fastfood_manager.entity.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+
+public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
+    // JpaRepository đã có sẵn các hàm CRUD: findAll(), findById(), save(), deleteById()
+
 }
