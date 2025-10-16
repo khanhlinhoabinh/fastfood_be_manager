5fa0fa098305e6bedccd1ad5936d794e37e751ed
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-10-16 11:23:40 +0700

    sap xep mon

--- src/main/java/com/fourctc/api_fastfood_manager/service/MenuItemService.java
+++ src/main/java/com/fourctc/api_fastfood_manager/service/MenuItemService.java
@@ -4,6 +4,8 @@ import com.fourctc.api_fastfood_manager.entity.MenuItem;
 import com.fourctc.api_fastfood_manager.repository.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Sort;
+
 import java.util.List;
 @Service
 public class MenuItemService {
@@ -17,5 +19,16 @@ public class MenuItemService {
      */
     public List<MenuItem> getAllMenuItems() {
         return menuItemRepository.findAll();
+    }
+
+    // hàm sắp xếp
+    public List<MenuItem> getSortedMenuItems(String field, String order) {
+        Sort sort = Sort.by(field);
+        if ("desc".equalsIgnoreCase(order)) {
+            sort = sort.descending();
+        } else {
+            sort = sort.ascending();
+        }
+        return menuItemRepository.findAll(sort);
     }
 }
