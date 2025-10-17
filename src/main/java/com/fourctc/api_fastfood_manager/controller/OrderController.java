1bec4b75ac96da5dcbe93ed3f23901ceb6eee5de
Lam. Nguyen Quang - CMC Global TTS <nqlam1@cmcglobal.vn>
2025-10-17 15:54:33 +0700

    Thêm chức năng xoá đơn hàng

--- src/main/java/com/fourctc/api_fastfood_manager/controller/OrderController.java
+++ src/main/java/com/fourctc/api_fastfood_manager/controller/OrderController.java
@@ -2,7 +2,10 @@ package com.fourctc.api_fastfood_manager.controller;
 
 import com.fourctc.api_fastfood_manager.entity.Order;
 import com.fourctc.api_fastfood_manager.service.OrderService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,4 +24,15 @@ public class OrderController {
         List<Order> orders = orderService.getAllOrders();
         return orders;  // Trả về các đơn hàng bao gồm các thông tin chi tiết
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteOrder(@PathVariable Integer id) {
+        try {
+            orderService.deleteOrderById(id);
+            return ResponseEntity.ok("Order deleted successfully.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
 }
--- src/main/java/com/fourctc/api_fastfood_manager/service/OrderService.java
+++ src/main/java/com/fourctc/api_fastfood_manager/service/OrderService.java
@@ -2,10 +2,13 @@ package com.fourctc.api_fastfood_manager.service;
 
 import com.fourctc.api_fastfood_manager.entity.Order;
 import com.fourctc.api_fastfood_manager.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -17,4 +20,17 @@ public class OrderService {
     public List<Order> getAllOrders() {
         return orderRepository.findAll();  // Lấy tất cả đơn hàng từ DB, sẽ tự động bao gồm cả orderDetails nếu đã được ánh xạ đúng
     }
+
+    @Transactional
+    public void deleteOrderById(Integer id) {
+        Optional<Order> orderOptional = orderRepository.findById(id);
+        if (orderOptional.isPresent()) {
+            Order order = orderOptional.get();
+            order.getPromotions().clear(); // Xoá liên kết với promotions
+            orderRepository.delete(order); // Xoá order và các liên kết cascade
+        } else {
+            throw new EntityNotFoundException("Order not found with ID: " + id);
+        }
+    }
+
 }
