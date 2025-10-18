2a45df51291fadf805b7fcacdec1eb4f46cf0ab5
DarkinTran <darkintran2005@gmail.com>
2025-10-18 21:44:30 +0700

    them tinh nang phan trang

--- src/main/java/com/fourctc/api_fastfood_manager/controller/OrderDetailController.java
+++ src/main/java/com/fourctc/api_fastfood_manager/controller/OrderDetailController.java
@@ -3,6 +3,7 @@ package com.fourctc.api_fastfood_manager.controller;
 import com.fourctc.api_fastfood_manager.dto.OrderDetailDTO;
 import com.fourctc.api_fastfood_manager.service.OrderDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +27,13 @@ public class OrderDetailController {
             @RequestParam(defaultValue = "quantity") String sortBy, // Mặc định sắp xếp theo mã món
             @RequestParam(defaultValue = "asc") String direction) {  // Mặc định sắp xếp tăng dần
         return orderDetailService.getAllOrderDetails(sortBy, direction);
+    }
+    // Lấy danh sách chi tiết đơn hàng với phân trang
+    @GetMapping("/paged")
+    public Page<OrderDetailDTO> getAllOrderDetails(
+            @RequestParam(defaultValue = "1") int page, // Trang bắt đầu từ 1
+            @RequestParam(defaultValue = "10") int size // Mặc định là 10 bản ghi mỗi trang
+    ) {
+        return orderDetailService.getOrderDetails(page, size);
     }
 }
