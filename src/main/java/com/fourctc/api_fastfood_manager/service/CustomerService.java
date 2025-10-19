c733b622345a074a396350cd87d0a6f5c9ae4217
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-10-19 16:15:04 +0700

    thempagechange

--- src/main/java/com/fourctc/api_fastfood_manager/service/CustomerService.java
+++ src/main/java/com/fourctc/api_fastfood_manager/service/CustomerService.java
@@ -5,6 +5,7 @@ import com.fourctc.api_fastfood_manager.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.*;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -103,4 +104,9 @@ public class CustomerService {
         return false;
 
     }
+    public Page<Customer> getCustomersPage(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
+        return customerRepository.findAll(pageable);
+    }
+
 }
